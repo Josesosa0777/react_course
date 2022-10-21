4c200755972f3a09245ba2e2995882071667c01e
Jose Sosa <josephsosa0777@gmail.com>
2022-10-21 12:59:04 -0500

    to do list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-import React from "react";
-import Header from "./Header";
-import Note from "./Note";
-import Footer from "./Footer";
+import React, {useState} from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
+    const [items, setItems] = useState([]);
+
+    function addItem(inputText) {
+        setItems(prevItems => {
+            return [...prevItems, inputText];
+        });
+    }
+
+    function deleteItem(id) {
+        setItems(prevItems => {
+            return prevItems.filter((item, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
-        <div>
-            <Header />
-            <Note />
-            <Footer />
+        <div className="container">
+        <div className="heading">
+            <h1>To-Do List</h1>
         </div>
-    )
+        <InputArea onAdd={addItem} />
+        <div>
+            <ul>
+            {items.map((todoItem, index) => (
+                <ToDoItem
+                key={index}
+                id={index}
+                text={todoItem}
+                onChecked={deleteItem}
+                />
+            ))}
+            </ul>
+        </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,26 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
- 
-const name = "Jose"
-const date = new Date();
-const hour = date.getHours()
-const currentyear = date.getFullYear();
-const img = "https://picsum.photos/200"
-const customStyle = { color: "green" };
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-let greeting = [];
-
-if (hour < 12) {
-  greeting = "good morning"
-  customStyle.color = "yellow"
-} else {
-  greeting = "good afternoon/night"
-  customStyle.color = "brown"
-}
-
 
 root.render(
     <App />
