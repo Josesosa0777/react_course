785c63d0329b58c7644f7b3a2bd10e23645f3687
Jose Sosa <josephsosa0777@gmail.com>
2022-10-21 10:51:25 -0500

    mapping data in components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,17 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+    return (
+        <Card 
+            key={contact.id}
+            name={contact.name}
+            img={contact.imgURL}
+            tel={contact.tel}
+            email={contact.email}
+        />
+    )
+}
 
 function App() {
     console.log(contacts)
@@ -10,12 +21,7 @@ function App() {
         <div>
             <h1 className="heading">My Contacts</h1>
             <Avatar img="https://i1.rgstatic.net/ii/profile.image/725175947501576-1549906827489_Q128/Jose_Sosa23.jpg"/>
-            <Card
-                name={contacts[0].name}
-                img={contacts[0].imgURL}
-                tel={contacts[0].phone}
-                email={contacts[0].email}
-            />
+            {contacts.map(createCard)}
         </div>
     )
 }
